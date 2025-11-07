ec3d7374724646a3030399bc90d1fc7f80c7a290
josue Huayapa <huayapajosue0@gmail.com>
2025-11-06 20:16:18 -0500

    CRUD users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $collection->string('email')->unique();
             $collection->timestamp('email_verified_at')->nullable();
             $collection->string('password');
-            $collection->string('api_token');
+            $collection->string('api_token')->nullable();
             $collection->enum('auth_provider', ['standard', 'google', 'github'])->default('standard');
             $collection->rememberToken();
             $collection->timestamps();
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,19 @@
 <?php
 
 use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth.api')->group(function () {
+    // CERRAR SESIÓN
     Route::post('/logout', [ProfileController::class, 'logout']);
+    // CRUD USUARIO
+    Route::get('/users', [UserController::class, 'getUsers']);
+    Route::get('/users/{id}', [UserController::class, 'getUser']);
+    Route::post('/users', [UserController::class, 'create']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
 });
 
 Route::post('/register', [ProfileController::class, 'register']);
